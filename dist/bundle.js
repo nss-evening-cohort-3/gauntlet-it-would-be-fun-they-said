8940cbd0b0b6dc7bc5a503ccef521c8c57621f30
MATT BRUTON <the.bruton@gmail.com>
2016-04-23 14:53:34 -0500

    fixed attack functions to subtract from player health

--- dist/bundle.js
+++ dist/bundle.js
@@ -26,7 +26,7 @@ $(".className").click(function(e) {
      // if (e.target.parentNode.classList.contains("className") || e.target.closest('className')) {
        PlayerOne.class = this.querySelector('.btn__text').innerHTML;
        console.log(this.querySelector('.btn__text').innerHTML);
-
+    $('#battleground').show();
      }
    })
  
@@ -46,7 +46,7 @@ $('#weapon-select').click(function(e) {
   }    
 })
 
-$(document).ready(function() {
+// $(document).ready(function() {
   // Function to check p1 and p2 current health
   function checkHealth(){
     // Checking to see if P1 is dead
@@ -61,25 +61,27 @@ $(document).ready(function() {
     else {
     // Call to function that calculate attack damages
     pTwoAttack();
+
     }
   }
 
 
   function pTwoAttack(){
-    p1RemHealth = PlayerOne.health - PlayerTwo.weapon.damage;
-
+    p1RemHealth = PlayerOne.health -= PlayerTwo.weapon.damage;
     pOneAttack(p1RemHealth);
+
+    playerOne.health = p1RemHealth;
   } 
 
   
   function pOneAttack(p1RemHealth){
-    p2RemHealth = PlayerTwo.health - PlayerOne.weapon.damage;
+    p2RemHealth = PlayerTwo.health -= PlayerOne.weapon.damage;
 
     atkStrings(p1RemHealth, p2RemHealth);
   } 
 
   function atkStrings(p1RemHealth, p2RemHealth){
-    p2AtkString = `${PlayerTwo.name} attacks with ${PlayerTwo.weapon} for ${PlayerTwo.weapon.damage} damage!`
+    p2AtkString = `${PlayerTwo.playerName} attacks with ${PlayerTwo.weapon} for ${PlayerTwo.weapon.damage} damage!`
     p1AtkString = `${PlayerOne.name} attacks with ${PlayerOne.weapon} for ${PlayerOne.weapon.damage} damage!`
     
 
@@ -164,7 +166,7 @@ $(document).ready(function() {
     $("." + previousCard).show();
   });
 checkHealth();
-});
+// });
 },{}],2:[function(require,module,exports){
 /*
   TODO: Modularize this code with IIFE or Browserify
