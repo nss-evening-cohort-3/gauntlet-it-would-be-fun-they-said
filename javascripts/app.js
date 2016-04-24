5096be76b3f0858d853ee6bab3cee7392f2de4fa
MATT BRUTON <the.bruton@gmail.com>
2016-04-24 12:40:03 -0500

    fixed the win and lose functions to display player name instead of undefined object

--- javascripts/app.js
+++ javascripts/app.js
@@ -103,14 +103,14 @@ function atkDisplay(p2AtkString, p1AtkString){
 
 loseGame = function(){
   $("#attackButton").hide();
-  alert($("PlayerTwo.name") + " Wins the Game");
+  alert(`${PlayerTwo.name} Wins the Game"`);
 }
 
 
 
 winGame = function (){
  $("#attackButton").hide();
- alert($("PlayerOne.name" ) + " Wins the Game");
+ alert(`${PlayerOne.name} Wins the Game`);
 }
 
   // Grab value from input for Player Name on click of anchor element "Select Class"
