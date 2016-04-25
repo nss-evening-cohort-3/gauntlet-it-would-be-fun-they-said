887a72b3da4ddf921a62971bea27e1284ffdc4f1
Bradley Guthrie <guppy96@gmail.com>
2016-04-25 18:22:45 -0500

    weapons converted and added iffe

--- javascripts/weapons.js
+++ javascripts/weapons.js
@@ -1,6 +1,19 @@
-var Gauntlet = Gauntlet || {};
-Gauntlet.Combatants = {};
+// var originalGauntlet()
 
+var Gauntlet = (function(originalGauntlet) {
+
+// var originalGauntlet = originalGauntlet || {};
+// originalGauntlet.Combatants = {};
+originalGauntlet.WeaponRack = {};
+
+originalGauntlet.WeaponRack.Weapon = function () {
+  this.name = "bare hands";
+  this.damage = 1;
+  this.hands = 2;
+  this.toString = function() {
+    return this.name;
+  }
+};
 
 var Weapon = function() {
   this.name = "bare hands";
@@ -11,25 +24,31 @@ var Weapon = function() {
     return this.name;
   }
 };
+console.log(originalGauntlet.WeaponRack.Weapon);
 
-var Dagger = function() {
+originalGauntlet.WeaponRack.Dagger =function() {
   this.name = "dagger";
   this.damage = 4;
   this.hands = 1;
 };
-Dagger.prototype = new Weapon();
+originalGauntlet.WeaponRack.Dagger.prototype = new originalGauntlet.WeaponRack.Weapon();
+console.log(originalGauntlet.WeaponRack.Dagger);
 
-var BroadSword = function() {
+originalGauntlet.WeaponRack.BroadSword = function() {
   this.name = "broad sword";
   this.damage = 14;
   this.hands = 2;
 };
-BroadSword.prototype = new Weapon();
+originalGauntlet.WeaponRack.BroadSword.prototype = new originalGauntlet.WeaponRack.Weapon();
+console.log(originalGauntlet.WeaponRack.BroadSword);
 
-var WarAxe = function() {
+originalGauntlet.WeaponRack.WarAxe = function() {
   this.name = "war axe";
   this.damage = 18;
   this.hands = 2;
 };
+originalGauntlet.WeaponRack.WarAxe.prototype = new originalGauntlet.WeaponRack.Weapon();
+console.log(originalGauntlet.WeaponRack.WarAxe);
 
-WarAxe.prototype = new Weapon();
+return originalGauntlet;
+} (Gauntlet || {}));
